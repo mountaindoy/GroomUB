be3350f130e438e230fb64a1eb077a979716be2d
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2022-01-03 17:10:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config(object):
+    API_HASH = os.getenv("API_HASH")
+    API_ID = int(os.getenv("API_ID"))
+    HEROKU_API = os.getenv("HEROKU_API")
+    HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+    SESSION = os.getenv("PYROGRAM_SESSION")
+    PREFIX = os.environ.get("PREFIX", ".")
+    SUDO = list(map(int, getenv("SUDO").split()))
+    LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
+    SUPPORT_GROUP = getenv("GROUP_SUPPORT", "GroomUB")
+    UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "x3Support")
+    UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/x3Network/GroomUB")
+    groom_version = "beta1.1"
---
+++ startup.sh
@@ -0,0 +1,2 @@
+echo STARTING GROOM UB
+python3 -m Groom
