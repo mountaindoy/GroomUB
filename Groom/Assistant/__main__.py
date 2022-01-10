479f81b8b6c0b7193c54c289c2bb36b03a3eb378
Wolverine <95951902+WolverinexD@users.noreply.github.com>
2022-01-10 22:10:20 +0530

    Update __main__.py

--- Groom/Assistant/__main__.py
+++ Groom/Assistant/__main__.py
@@ -1,1 +1,10 @@
+from pyrogram import Client, filters
+from Groom import bot
+from config import Config
+from pyrogram.types import Message
+from Groom import USER_NAME
 
+
+@bot.on_message(filters.command("start"))
+def bot_start(client: Client, message: Message):
+  await send_message("This is Groom UB Assistant")
